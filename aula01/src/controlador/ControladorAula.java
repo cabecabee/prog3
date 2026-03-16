0957e3bcb3b92447464e2ceb71364a6a1f138fa0
cabeca <bernardomb004@gmail.com>
2026-03-16 11:23:42 -0300

    Add files via upload

---
+++ aula01/src/controlador/ControladorAula.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Discentes
+ */
+public class ControladorAula {
+
+    public ControladorAula(JTextField jTextFieldN, JLabel jLabelSaida) {
+        this.jTextFieldN = jTextFieldN;
+        this.jLabelSaida = jLabelSaida;
+    }
+    
+    JTextField jTextFieldN;
+    JLabel jLabelSaida;
+}
